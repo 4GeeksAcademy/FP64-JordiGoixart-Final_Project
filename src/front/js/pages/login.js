33734e5cb569f061664425323d9e649e86194447
jordidel170 <157019626+jordidel170@users.noreply.github.com>
2024-06-06 08:45:19 +0000

    testeando 2

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -37,15 +37,15 @@ export const Login = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			{/* {token && token != "" && token != undefined ? (
+			{token && token != "" && token != undefined ? (
 				"You are logged in with this token" + token
-			 ) : ( */}
+			 ) : (
 				<div>
 					<input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
 					<input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
 					<button onClick={handleClick}>Login</button>
 				</div>
-			{/* )} */}
+			)}
 		</div>
 	);
 };
